c06f86edb2345572aa205eb9c4b4d2a29c45c161
Mykola <mykola.kushnir@gmail.com>
2023-09-16 23:36:17 +0300

    UC#1 Create endpoint. Added initial controller and endpoint implementation.

---
+++ GenAIUseCase1/Controllers/CountriesController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+using System.Xml.Linq;
+
+namespace GenAIUseCase1.Controllers {
+
+	[Route("api/[controller]")]
+	[ApiController]
+	public class CountriesController : ControllerBase {
+
+		private readonly IHttpClientFactory _httpClientFactory;
+
+		public CountriesController(IHttpClientFactory httpClientFactory)
+		{
+			_httpClientFactory = httpClientFactory;
+		}
+
+		[HttpGet("GetAllCountries")]
+		public async Task<IActionResult> GetAllCountries(string countryName, string countryPopulation, string sortType, int limit)
+		{
+			try {
+				var apiUrl = "https://restcountries.com/v3.1/all";
+				var httpClient = _httpClientFactory.CreateClient();
+
+				var response = await httpClient.GetAsync(apiUrl);
+
+				if (!response.IsSuccessStatusCode) {
+					return StatusCode((int)response.StatusCode);
+				}
+
+				var contentStream = await response.Content.ReadAsStreamAsync();
+				var countries = await JsonSerializer.DeserializeAsync<object>(contentStream);
+
+				return Ok(countries);
+			}
+			catch (Exception ex) {
+				return StatusCode(500, ex.Message);
+			}
+		}
+
+	}
+
+
+}
--- GenAIUseCase1/Program.cs
+++ GenAIUseCase1/Program.cs
@@ -6,6 +6,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddHttpClient();
 
 var app = builder.Build();
 
@@ -17,6 +18,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseRouting();
 
 app.UseAuthorization();
 
